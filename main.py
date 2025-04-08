676ac97156f615d7cea17bc545de8925bde8746b
giuseppe <chichizolapepe@gmail.com>
2025-04-08 17:42:25 -0500

    word count added

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-print("greetings boots")
+
+def get_book_text(filepath):
+
+    with open(filepath) as f:
+        return f.read()
+
+
+
+def main():
+    
+    path = "books/frankenstein.txt"
+    text = get_book_text(path)
+    # print(text)
+
+    num_words = len(text.split())
+    print(f"{num_words} words found in the document")
+
+
+main()
